56357bdf58e59fe6bf8cf8ed3451d9f545e52637
Naval Kishor Saw <nksaw15@gmail.com>
2026-06-17 18:54:42 +0530

    Updated config

--- src/config.py
+++ src/config.py
@@ -3,17 +3,17 @@ from pathlib import Path
 def get_config():
     return {
         'lr': 3e-4, 
-        "d_model": 256,
+        "d_model": 512,
         "heads_num": 4,
         "dropout": 0.1,
         "num_layers": 4,
-        "src_seq_len": 512 + 2, # +2 for [BOS] and [EOS]
-        "tgt_seq_len": 80 + 1, # +1 for [EOS]
-        "batch_size": 8,
+        "src_seq_len": 320,
+        "tgt_seq_len": 72,
+        "batch_size": 16,
         "model_folder": "weights",
         "model_basename": "tmodel_",
         "preload": None,
-        'num_epochs': 20,
+        'num_epochs': 25,
         "tokenizer_path": "models/tokenizer.json",
         "experiment_name": "runs/tmodel",
         "warmup_ratio": 0.10
